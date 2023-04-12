4b1193101dcc02317fbfa915c51604e462f941c0
Prajal Gupta <gupta.prajal@gmail.com>
2023-04-12 09:08:30 +0530

    Cart add [product

--- src/Components/Cart/CartItem/CartItem.tsx
+++ src/Components/Cart/CartItem/CartItem.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import ProductList from "../../ProductList/ProductList";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { RemoveProduct} from '../../../store/actions/cart';
+import { AddProduct, RemoveProduct} from '../../../store/actions/cart';
 import {Product} from '../../../models/Product';
 import { useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk'
@@ -40,20 +40,25 @@ const CartItem: React.FC<Props> = ({image, title, type, description, height, wid
         <div style={{height: '100%', width: '20%'}}>
         <Card.Img variant="top" src = {image} style={{height: '100%', width: '100%'}}/>
         </div>
-        <div style={{height: '100%', width: '70%'}} >
+        <div style={{height: '100%', width: '80%'}} >
         <Card.Body>
             <Card.Title>{title}</Card.Title>
             <Card.Text>{description}</Card.Text>
             <Card.Text>Price = {price}</Card.Text>
-            <Card.Text>Quantity: {amount}</Card.Text>
+            <Card.Text>Quantity: 
+              <Button variant="primary" style={{backgroundColor: 'black', color: 'white'}} onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(AddProduct(currentProduct));
+              }}>
+              +
+              </Button> {amount}
+              <Button variant="primary" style={{backgroundColor: 'black', color: 'white'}} onClick={(e) => {
+                e.preventDefault();
+                dispatch(RemoveProduct(currentProduct));
+              }}> -
+              </Button>
+            </Card.Text>
         </Card.Body>
-        </div>
-        <div style={{height: '100%', width: '10%' ,margin: '10px'}}>
-          <Button variant="primary" onClick={(e) => {
-            e.preventDefault();
-            dispatch(RemoveProduct(currentProduct));
-        }}>Remove
-        </Button>
         </div>
     </Card>
   );
